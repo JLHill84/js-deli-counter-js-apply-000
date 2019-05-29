048c26bf3b79eaacca05fc0c2907b2395ffddde0
Joshua Hill <josh@jlhworkshop.com>
2019-05-29 04:15:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function currentLine(line) {
   if (!line.length) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
 
-  const numbersAndNames = []
+  const numbersAndNames = [];
 
   for (let i = 0, l = line.length; i < l; i++) {
-    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+    numbersAndNames.push(`${i + 1}. ${line[i]}`);
   }
 
-  return `The line is currently: ${numbersAndNames.join(', ')}`
-};
+  return `The line is currently: ${numbersAndNames.join(', ')}`;
+}
 
 function nowServing(line) {
   if (!line.length) {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }
 
-  return `Currently serving ${line.shift()}.`
-};
+  return `Currently serving ${line.shift()}.`;
+}
 
 function takeANumber(line, name) {
   line.push(name)
 
   return `Welcome, ${name}. You are number ${line.length} in line.`
-};
+}
