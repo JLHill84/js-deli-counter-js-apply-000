32677df18f91038c2944e3b25faed2a591d01098
Joshua Hill <josh@jlhworkshop.com>
2019-05-29 04:18:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function currentLine(katzDeliLine) {
-  if (!line.length) {
+  if (!katzDeliLine.length) {
     return "The line is currently empty.";
   }
 
   const numbersAndNames = [];
 
-  for (let i = 0, l = line.length; i < l; i++) {
+  for (let i = 0, l = katzDeliLine.length; i < l; i++) {
     numbersAndNames.push(`${i + 1}. ${katzDeliLine[i]}`);
   }
 
@@ -13,15 +13,15 @@ function currentLine(katzDeliLine) {
 }
 
 function nowServing(katzDeliLine) {
-  if (!line.length) {
+  if (!katzDeliLine.length) {
     return "There is nobody waiting to be served!";
   }
 
-  return `Currently serving ${line.shift()}.`;
+  return `Currently serving ${katzDeliLine.shift()}.`;
 }
 
-function takeANumber(line, name) {
-  line.push(name)
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
 
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
