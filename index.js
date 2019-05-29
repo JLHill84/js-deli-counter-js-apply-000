94c0f2f77a02a302213a5a71c19828d2eb9d0098
Joshua Hill <josh@jlhworkshop.com>
2019-05-29 04:05:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ function takeANumber (katzDeliLine,Name) {
   return "Welcome, " + Name + ". You are number " + katzDeliLine.length + " in line.";
 }
 
-function nowServing () {
+function nowServing (katzDeliLine) {
+  if katzDeliLine.length > 0 {
+      katzDeliLine.pop()
+  }
 takeANumber(katzDeliLine, "Ada");
 takeANumber(katzDeliLine, "Grace");
 takeANumber(katzDeliLine, "Kent");
