a3362c6285d6c30f810dce43ff4cd544b3df00e5
Joshua Hill <josh@jlhworkshop.com>
2019-05-29 04:01:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber (katzDeliLine,Name) {
   katzDeliLine.push(Name);
-  return "Welcome, " + Name + ". You are number" + katzDeliLine.length+one + " in line.";
+  return "Welcome, " + Name + ". You are number" + katzDeliLine.length + " in line.";
 }
 
 function
